acef6538bfebd21f2630ffd08811af752f5b69fa
srikarn <srikar.n@comakeit.com>
2019-09-16 14:13:13 +0530

    added comments

--- services-example/src/main.ts
+++ services-example/src/main.ts
@@ -1,3 +1,8 @@
+/* main.ts - The main.ts file acts as the main entry point of our Angular application. This file is responsible for the bootstrapper operation of our Angular modules. It contains some important statements related to the modules and some initial setup configurations like 
+enableProdMode – This option is used to disable Angular’s development mode and enable Productions mode. Disabling Development mode turns off assertions and other model-related checks within the framework.
+platformBrowserDynamic – This option is required to bootstrap the Angular app n the browser.
+AppModule – This option indicates which module acts as a root module in the applications.
+environment – This option stores the values of the different environment constants.  */
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
