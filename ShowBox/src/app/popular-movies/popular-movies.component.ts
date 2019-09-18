49e834998c491bcc5faf57a179c44b7ae4dff528
Srikar <srikarnanduri628@gmail.com>
2019-09-18 23:09:58 +0530

    changed to scss

--- ShowBox/src/app/popular-movies/popular-movies.component.ts
+++ ShowBox/src/app/popular-movies/popular-movies.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sb-popular-movies',
   templateUrl: './popular-movies.component.html',
-  styleUrls: ['./popular-movies.component.css']
+  styleUrls: ['./popular-movies.component.scss']
 })
 export class PopularMoviesComponent implements OnInit {
 
