377e13bbd5a841b6dea2a058ceff23c6189ee5b7
Srikar <srikarnanduri628@gmail.com>
2019-10-09 22:19:40 +0530

    handled manually entering url to redirect back to popular movies.

--- ShowBox/src/app/components/movie-details/movie-details.component.ts
+++ ShowBox/src/app/components/movie-details/movie-details.component.ts
@@ -21,17 +21,31 @@ export class MovieDetailsComponent implements OnInit {
   constructor(private movieDetailsService: MovieDetailsService, public activatedRoute: ActivatedRoute) { }
   ngOnInit() {
 
-    this.activatedRoute.paramMap.pipe(
+    // this here shows how to get data from the router state.
+/*     this.activatedRoute.paramMap.pipe(
       map(() => window.history.state)
     ).subscribe(result => {
       console.log('movieDetailsComponent ' + result.movieId);
       this.movieId = result.movieId;
-    });
+    }); */
 
     this.getMovieDetails();
   }
 
   getMovieDetails() {
+/*  this method is used to impliment resolve interface so that we can redirect to popular-movies
+ page if there is no Id to show the details in details page. if we have the id we directly get the data from there
+ it is a service that makes the rest call. */
+    this.activatedRoute.data.subscribe((data: {details: MovieDetailsModel.MovieDetails}) => {
+      this.movieDetails = data.details;
+      this.genres = this.movieDetails.genres;
+      this.similar = this.movieDetails.similar.results.slice(0, 5);
+      this.loading = false;
+    });
+  }
+
+  passId(id) {
+    this.movieId = id;
     this.movieDetailsService.getMovieDetails(this.movieId).subscribe(
       result => {
         this.movieDetails = result;
@@ -41,19 +55,5 @@ export class MovieDetailsComponent implements OnInit {
       },
       err => this.errorMessage = err
     );
-
-// tslint:disable-next-line: max-line-length
-// this method is used to impliment resolve interface so that we can redirect to popular-movies page if there is no Id to show the details in details page.
-  /*   this.activatedRoute.data.subscribe((data: {details: MovieDetailsModel.MovieDetails}) => {
-      this.movieDetails = data.details;
-      this.genres = this.movieDetails.genres;
-      this.similar = this.movieDetails.similar.results.slice(0, 5);
-      this.loading = false;
-    }); */
-  }
-
-  passId(id) {
-    this.movieId = id;
-    this.getMovieDetails();
   }
 }
--- ShowBox/src/app/guards/detailsResolverService.service.ts
+++ ShowBox/src/app/guards/detailsResolverService.service.ts
@@ -5,7 +5,7 @@ undefined error inside the router. So this is not working for our case.
  */
 
 
-/* import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Resolve, Router, ActivatedRoute } from '@angular/router';
 import { Observable, of, EMPTY } from 'rxjs';
 import { MovieDetailsService } from '../services/movie-details-service/movie-details.service';
@@ -22,34 +22,34 @@ export class DetailsResolverService implements Resolve<MovieDetailsModel.MovieDe
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
     MovieDetailsModel.MovieDetails | Observable<MovieDetailsModel.MovieDetails> | Promise<MovieDetailsModel.MovieDetails> {
 
-    Here we are trying to get data from the router, the data is inside extras of xurrentNavigation.
-    We are sending the data through navigateByUrl and inside state object. so here it's still navigating
-    to the url so we can find data inside router, once it's gone to the new url we need to gget the data from history.
+    /*  Here we are trying to get data from the router, the data is inside extras of xurrentNavigation.
+     We are sending the data through navigateByUrl and inside state object. so here it's still navigating
+     to the url so we can find data inside router, once it's gone to the new url we need to gget the data from history. */
 
-/* Official doc says:
+    /* Official doc says:
 
-  state?: {
-    [k: string]: any;
-}
-Developer-defined state that can be passed to any navigation. Access this value through the Navigation.extras object returned from
-router.getCurrentNavigation() while a navigation is executing.
+      state?: {
+        [k: string]: any;
+    }
+    Developer-defined state that can be passed to any navigation. Access this value through the Navigation.extras object returned from
+    router.getCurrentNavigation() while a navigation is executing.
 
-After a navigation completes, the router writes an object containing this value together with a navigationId to history.state.
-The value is written when location.go() or location.replaceState() is called before activating this route.
+    After a navigation completes, the router writes an object containing this value together with a navigationId to history.state.
+    The value is written when location.go() or location.replaceState() is called before activating this route.
 
-Note that history.state does not pass an object equality test because the router adds the navigationId on each navigation. */
+    Note that history.state does not pass an object equality test because the router adds the navigationId on each navigation. */
 
-    /* const id = this.router.getCurrentNavigation().extras.state.movieId;
+    if (this.router.getCurrentNavigation().extras.state) {
+    const id = this.router.getCurrentNavigation().extras.state.movieId;
     console.log('resolver ' + id);
     return this.movieDetails.getMovieDetails(id).pipe(
       mergeMap(details => {
-        if (details) {
           return of(details);
-        } else {
-          this.router.navigate(['/popular-movies']);
-          return EMPTY;
-        }
       })
     );
+  } else {
+    this.router.navigate(['/popular-movies']);
+    return EMPTY;
   }
-} */
+}
+}
